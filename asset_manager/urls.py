9b7bb5b183eb612117fabd2b85971b8f87b4afc0
omariut <omarcpgcbl@gmail.com>
2022-11-29 13:08:58 +0600

    server up

--- asset_manager/urls.py
+++ asset_manager/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.urls import path
-from product import views
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-from config import settings
+from asset_manager import settings
 from django.urls.conf import re_path
 from django.conf.urls.static import static
 
